c1a7ff9f2a2c512765c187978ec0a8c6e631c761
Mason Kim <qwerty3345@naver.com>
2023-08-24 03:48:29 +0900

    [Feat] 검색 상태에 따른 다른 뷰 표시 구현

--- AppStoreSearch/Sources/View/Search/SearchView.swift
+++ AppStoreSearch/Sources/View/Search/SearchView.swift
@@ -7,28 +7,24 @@
 
 import SwiftUI
 
+enum SearchState {
+  case recent
+  case suggestion
+  case result
+}
+
 struct SearchView: View {
-  @State private var searchText = ""
-  let mock = ["녹음기", "엠넷", "1234", "63146", "234", "234", "23467", "2354", "134", "6345", "엠넷", "진에어", "녹음기", "엠넷", "진에어", "녹음기", "엠넷", "진에어", "녹음기", "엠넷", "진에어", "녹음기", "엠넷", "진에어"]
+  @State private var searchText = "aa"
+  @State private var searchState: SearchState = .recent
+
+  let mockRecentSearch = ["최근검색1", "최근검색2"]
+  let mockSuggestion = ["제안1", "제안2"]
+  let mockSearchResult = ["검색결과1", "검색결과2"]
 
   var body: some View {
     NavigationStack {
-      Section {
-        List(mock, id: \.self) { data in
-          Button {
-            // TODO: 여기서 특정 아이템이 눌렸을 때, searchable이 trigger되게...
-            searchText = data
-          } label: {
-            Text(data)
-              .foregroundColor(.blue)
-              .font(.title3)
-          }
-        }
-        .listStyle(.plain)
-      } header: {
-        recentSearchTitle
-      }
-      .navigationTitle("검색")
+      showingList
+        .navigationTitle("검색")
     }
     .searchable(
       text: $searchText,
@@ -36,17 +32,79 @@ struct SearchView: View {
       prompt: "App Store"
     )
     .onSubmit(of: .search) {
-      // TODO: 검색 시,
+      // TODO: 검색 시, Service에서 호출해서 받아오게 구현
+      searchState = .result
+    }
+    .onChange(of: searchText) { newValue in
+      searchState = searchText.isEmpty ? .recent : .suggestion
     }
   }
 
-  var recentSearchTitle: some View {
+  @ViewBuilder
+  private var showingList: some View {
+    switch searchState {
+    case .recent:
+      recentSearchList
+    case .suggestion:
+      suggestionList
+    case .result:
+      searchResultList
+    }
+  }
+
+  private var recentSearchList: some View {
+    Section {
+      List(mockRecentSearch, id: \.self) { data in
+        Button {
+          // TODO: 여기서 특정 아이템이 눌렸을 때, searchable이 trigger되게...
+          searchText = data
+        } label: {
+          Text(data)
+            .foregroundColor(.blue)
+            .font(.title3)
+        }
+      }
+      .listStyle(.plain)
+    } header: {
+      recentSearchTitle
+    }
+  }
+
+  private var recentSearchTitle: some View {
     Text("최근 검색어")
       .font(.title2)
       .fontWeight(.bold)
       .frame(maxWidth: .infinity, alignment: .leading)
       .padding(.horizontal)
       .padding(.top)
+  }
+
+  private var suggestionList: some View {
+    List(mockSuggestion, id: \.self) { data in
+      Button {
+        // TODO: 여기서 특정 아이템이 눌렸을 때, 실제 검색을 trigger
+        searchText = data
+      } label: {
+        Text(data)
+          .foregroundColor(.blue)
+          .font(.title3)
+      }
+    }
+    .listStyle(.plain)
+  }
+
+  private var searchResultList: some View {
+    List(mockSearchResult, id: \.self) { data in
+      Button {
+        // TODO: 여기서 특정 아이템이 눌렸을 때, 실제 검색을 trigger
+        searchText = data
+      } label: {
+        Text(data)
+          .foregroundColor(.blue)
+          .font(.title3)
+      }
+    }
+    .listStyle(.plain)
   }
 }
 
