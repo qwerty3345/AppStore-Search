e7f44377dc4810ff1e13661b933d087fb77f03a6
Mason Kim <qwerty3345@naver.com>
2023-08-24 01:06:29 +0900

    [Build] 프로젝트 기본 생성

---
+++ ios-itunes-search/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-itunes-search
+//
+//  Created by Mason Kim on 2023/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
