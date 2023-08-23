2ec5c5f1fed9b5b1bea2498ab4138722b379dc22
Mason Kim <qwerty3345@naver.com>
2023-08-24 05:00:58 +0900

    [Feat] HTTP Task 정의
    - header, body의 활용 로직이 필요하다면 차후 구현 _ buildRequest 로직에서도 반영되어야 함

---
+++ AppStoreSearch/Sources/New Group/Network/Endpoint/HTTPTask.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPTask.swift
+//  AppStoreSearch
+//
+//  Created by Mason Kim on 2023/08/24.
+//
+
+import Foundation
+
+enum HTTPTask {
+  case requestPlain
+  /// header, body 활용 로직이 필요하다면 차후 구현
+}
