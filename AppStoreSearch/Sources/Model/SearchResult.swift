b9fe207df671a69a6a8922e4e9c83245f45f332e
Mason Kim <qwerty3345@naver.com>
2023-08-24 13:44:19 +0900

    [Refactor] SearchResult Identifiable 프로토콜 채택
    - 해당 DTO 타입을 그대로 View에서 사용하기 위해 채택.
    - id값은 앱 ID로 지정함

--- AppStoreSearch/Sources/Model/SearchResult.swift
+++ AppStoreSearch/Sources/Model/SearchResult.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 struct SearchResult: Decodable {
+  /// 앱 ID
+  let trackId: Int
   /// 앱 이름
   let trackName: String
   /// 앱 상세 설명
@@ -41,4 +43,8 @@ struct SearchResult: Decodable {
   let artworkUrl100: URL
   /// 512x512 로고 URL
   let artworkUrl512: URL
+}
+
+extension SearchResult: Identifiable {
+  var id: Int { trackId }
 }
