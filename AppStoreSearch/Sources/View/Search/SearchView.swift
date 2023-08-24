919f1ef3a2a5e116218b55ce42084a46a8186a4a
Mason Kim <qwerty3345@naver.com>
2023-08-24 12:47:31 +0900

    [Feat] 추천 검색어 Suggestion 기능 뷰 연결

--- AppStoreSearch/Sources/View/Search/SearchView.swift
+++ AppStoreSearch/Sources/View/Search/SearchView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 enum SearchState {
   case recent
-  case suggestion
   case result
 }
 
@@ -17,8 +16,12 @@ struct SearchView: View {
   @State private var searchText = ""
   @State private var searchState: SearchState = .recent
 
+  @State private var suggestions: [String] = []
+  @State private var currentSuggestionTask: Task<Void, Error>? = nil
+
+  let searchService = SearchService(router: NetworkRouter())
+
   let mockRecentSearch = ["최근검색1", "최근검색2"]
-  let mockSuggestion = ["제안1", "제안2", "제안3", "제안4"]
   let mockSearchResult = ["검색결과1", "검색결과2"]
 
   var body: some View {
@@ -31,12 +34,21 @@ struct SearchView: View {
       placement: .navigationBarDrawer(displayMode: .always),
       prompt: "App Store"
     )
+    .searchSuggestions {
+      suggestionView
+    }
     .onSubmit(of: .search) {
       // TODO: 검색 시, Service에서 호출해서 받아오게 구현
       searchState = .result
     }
-    .onChange(of: searchText) { newValue in
-      searchState = searchText.isEmpty ? .recent : .suggestion
+    .onChange(of: searchText) { searchText in
+      currentSuggestionTask?.cancel()
+
+      currentSuggestionTask = Task {
+        suggestions = try await searchService.suggestion(of: searchText)
+      }
+
+      searchState = .recent
     }
   }
 
@@ -45,8 +57,6 @@ struct SearchView: View {
     switch searchState {
     case .recent:
       recentSearchList
-    case .suggestion:
-      suggestionList
     case .result:
       searchResultList
     }
@@ -79,20 +89,16 @@ struct SearchView: View {
       .padding(.top)
   }
 
-  private var suggestionList: some View {
-    List(mockSuggestion, id: \.self) { data in
-      Button {
-        // TODO: 여기서 특정 아이템이 눌렸을 때, 실제 검색을 trigger
-        searchText = data
-      } label: {
-        HStack(spacing: 0) {
-          Text("")  // Image가 가장 앞에 있으면 Divider가 살짝 잘리는 이슈 때문에 추가함.
+  private var suggestionView: some View {
+    ForEach(suggestions, id: \.self) { suggestion in
+      HStack(spacing: 0) {
+        Text("")  // Image가 가장 앞에 있으면 Divider가 살짝 잘리는 이슈 때문에 추가함.
 
-          Image(systemName: "magnifyingglass")
-            .padding(.trailing, 8)
-          Text(data)
-        }
+        Image(systemName: "magnifyingglass")
+          .padding(.trailing, 8)
+        Text(suggestion)
       }
+      .searchCompletion(suggestion)
     }
     .listStyle(.plain)
   }
