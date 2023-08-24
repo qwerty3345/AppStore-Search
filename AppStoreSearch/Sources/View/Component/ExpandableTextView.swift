0eaa07f4847a942d70bcd8ac4c93cc5a0d48b2be
Mason Kim <qwerty3345@naver.com>
2023-08-24 20:28:31 +0900

    [Feat] 확장가능 텍스트뷰에서 잘리지 않았을 때는 더보기를 표시하지 않도록 구현
    - TextTruncatedCheckModifier 를 정의하여 활용
    - https://prafullkumar77.medium.com/swiftui-how-to-make-see-more-see-less-style-button-at-the-end-of-text-675f859c2c4f 참고

--- AppStoreSearch/Sources/View/Component/ExpandableTextView.swift
+++ AppStoreSearch/Sources/View/Component/ExpandableTextView.swift
@@ -10,16 +10,22 @@ import SwiftUI
 
 struct ExpandableTextView: View {
   let text: String
+  let lineLimit = 3
   @State private var isExpanded = false
+  @State private var isTruncated = false
 
   var body: some View {
     ZStack(alignment: .bottomTrailing) {
       Text(text)
-        .lineLimit(isExpanded ? nil : 3)
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .lineLimit(isExpanded ? nil : lineLimit)
+        .checkIsTruncated(
+          text: text,
+          withLineLimit: lineLimit,
+          isTruncated: $isTruncated
+        )
 
-      if !isExpanded {
-        Text(" 더 보기")
+      if !isExpanded && isTruncated {
+        Text(isTruncated ? " 더 보기" : "")
           .foregroundColor(.accentColor)
           .background(.white)
       }
@@ -32,9 +38,56 @@ struct ExpandableTextView: View {
   }
 }
 
+struct TextTruncatedCheckModifier: ViewModifier {
+  let text: String
+  let lineLimit: Int
+  @Binding var isTruncated: Bool
+
+  func body(content: Content) -> some View {
+    content
+      .background(
+        Text(text)
+          .lineLimit(lineLimit)
+          .background(
+            GeometryReader { visibleTextGeometry in
+              ZStack {
+                Text(text)
+                  .background(
+                    GeometryReader { fullTextGeometry in
+                      Color.clear.onAppear {
+                        // isTruncated 업데이트
+                        isTruncated = fullTextGeometry.size.height > visibleTextGeometry.size.height
+                      }
+                    }
+                  )
+              }
+              .frame(height: .greatestFiniteMagnitude)
+            }
+          )
+          .hidden()
+      )
+  }
+}
+
+private extension View {
+  /// 해당 텍스트가 lineLimit를 기준으로 잘리는지 아닌지를 확인하고 업데이트
+  func checkIsTruncated(
+    text: String,
+    withLineLimit lineLimit: Int,
+    isTruncated: Binding<Bool>
+  ) -> some View {
+    self.modifier(
+      TextTruncatedCheckModifier(
+        text: text,
+        lineLimit: lineLimit,
+        isTruncated: isTruncated
+      )
+    )
+  }
+}
 
 struct ExpandableTextView_Previews: PreviewProvider {
   static var previews: some View {
-    ExpandableTextView(text: String(repeating: "asdkajf kjsadfla sdk", count: 100))
+    ExpandableTextView(text: String(repeating: "asdkajf kjsadfla sdk", count: 6))
   }
 }
