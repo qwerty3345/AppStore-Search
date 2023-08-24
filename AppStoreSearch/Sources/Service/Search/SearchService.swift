9446540710f81a3f41328190be90f0afc1e072ef
Mason Kim <qwerty3345@naver.com>
2023-08-24 12:45:59 +0900

    [Feat] SearchService - Suggestion 구현

---
+++ AppStoreSearch/Sources/Service/Search/SearchService.swift
@@ -0,0 +1,33 @@
+//
+//  SearchService.swift
+//  AppStoreSearch
+//
+//  Created by Mason Kim on 2023/08/24.
+//
+
+import Foundation
+
+final class SearchService {
+
+  // MARK: - Properties
+
+  private let router: NetworkRouterProtocol
+
+  // MARK: - Initialization
+
+  init(router: NetworkRouterProtocol) {
+    self.router = router
+  }
+
+  // MARK: - Public Methods
+
+  func suggestion(of query: String) async throws -> [String] {
+    let suggestionResponse = try await router.request(
+      with: SearchEndpoint.suggestion(query: query),
+      type: SuggestionResponse.self
+    )
+
+    let suggestions = suggestionResponse.results.map { $0.trackName }
+    return suggestions
+  }
+}
