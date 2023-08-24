7a8f7a2f8ffd0c6b37570ee097b2fd777cd4bd9c
Mason Kim <qwerty3345@naver.com>
2023-08-25 03:06:12 +0900

    [Feat] UserDetaults Property Wrapper 구현

---
+++ AppStoreSearch/Sources/Core/Persistence/UserDefault.swift
@@ -0,0 +1,24 @@
+//
+//  UserDefault.swift
+//  AppStoreSearch
+//
+//  Created by Mason Kim on 2023/08/25.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+  private let key: String
+  private let defaultValue: T
+
+  init(key: String, defaultValue: T) {
+    self.key = key
+    self.defaultValue = defaultValue
+  }
+
+  var wrappedValue: T {
+    get { return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue }
+    set { UserDefaults.standard.set(newValue, forKey: key) }
+  }
+}
