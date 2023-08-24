e010ca8c529920accfe93500b358b11bdfc2fe97
Mason Kim <qwerty3345@naver.com>
2023-08-24 18:09:33 +0900

    [Refactor] 디테일 헤더부분 분리

--- AppStoreSearch/Sources/View/Detail/AppDetailView.swift
+++ AppStoreSearch/Sources/View/Detail/AppDetailView.swift
@@ -12,80 +12,14 @@ struct AppDetailView: View {
 
   var body: some View {
     VStack {
-      headerSection
+      AppDetailHeaderSection(result: result)
+        .padding(.bottom)
 
       Divider()
 
-      infoSection
+      AppDetailInfoSection(result: result)
 
       Divider()
-    }
-  }
-
-  private var headerSection: some View {
-    HStack {
-      RoundedRectangle(cornerRadius: 16)
-        .foregroundColor(.gray)
-        .frame(width: 126, height: 126)
-        .padding(.trailing)
-      //        AsyncImage(url: result.artworkUrl100)
-      //          .cornerRadius(16)
-
-      VStack {
-        VStack(alignment: .leading) {
-          Text(result.trackName)
-            .font(.title)
-            .fontWeight(.bold)
-          Text(result.artistName)
-            .foregroundColor(.gray)
-            .font(.body)
-            .fontWeight(.medium)
-        }
-        .frame(maxWidth: .infinity, alignment: .leading)
-
-        //          Spacer()
-
-        HStack {
-          Button {
-
-          } label: {
-            Text("받기")
-              .foregroundColor(.blue)
-              .fontWeight(.bold)
-              .frame(width: 80, height: 32)
-              .background(.gray.opacity(0.2))
-              .cornerRadius(16)
-          }
-
-          Spacer()
-
-          Button {
-            // 공유 액션
-          } label: {
-            Image(systemName: "square.and.arrow.up")
-              .resizable()
-              .scaledToFit()
-              .frame(width: 32, height: 32)
-              .fontWeight(.medium)
-          }
-        }
-      }
-    }
-    .padding(.horizontal)
-  }
-
-  private var infoSection: some View {
-    ScrollView(.horizontal, showsIndicators: false) {
-      HStack {
-        ratingInfoView
-      }
-    }
-  }
-
-  private var ratingInfoView: some View {
-    VStack {
-      Text("\(result.userRatingCountText)개의 평가")
-      Text("\(result.averageUserRating)")
     }
   }
 }
