a058b456ea7e2d52f060d88b0ee88285ab79ce14
Mason Kim <qwerty3345@naver.com>
2023-08-25 17:04:50 +0900

    [Feat] Carousel 뷰 구현
    - 앱스토어 형태로 우측의 컨텐츠를 약간 보여주며 페이징 스크롤 되게끔 처리
    - 전체 넓이 대비 드래그한 비율을 계산해서 가까운 Carousel 아이템으로 이동하는 형태로 구현

---
+++ AppStoreSearch/Sources/View/Component/Carousel.swift
@@ -0,0 +1,107 @@
+//
+//  Carousel.swift
+//  AppStoreSearch
+//
+//  Created by Mason Kim on 2023/08/25.
+//
+
+import SwiftUI
+
+/// 좌측으로 정렬하고 우측의 컨텐츠를 약간 보여주는 Carousel _ For AppStore
+struct Carousel<Content: View, T: Hashable>: View {
+
+  // MARK: - Properties
+
+  let content: (T) -> Content
+  let items: [T]
+
+  let spacing: CGFloat
+  let trailingSpace: CGFloat
+  @Binding var index: Int
+
+  @GestureState private var offset: CGFloat = 0
+  @State private var currentIndex: Int = 0
+
+  // MARK: - Initialization
+
+  init(
+    items: [T],
+    index: Binding<Int>,
+    spacing: CGFloat = 15,
+    trailingSpace: CGFloat = 100,
+    content: @escaping (T) -> Content
+  ) {
+    self.items = items
+    self._index = index
+    self.spacing = spacing
+    self.trailingSpace = trailingSpace
+    self.content = content
+  }
+
+  // MARK: - Body
+
+  var body: some View {
+    GeometryReader { geometry in
+      let width = geometry.size.width - (trailingSpace - spacing)
+
+      HStack(spacing: spacing) {
+        ForEach(items, id: \.self) { item in
+          content(item)
+            .frame(width: geometry.size.width - trailingSpace)
+        }
+      }
+      .padding(.horizontal, spacing)
+      .offset(x: (CGFloat(currentIndex) * -width) + offset)
+      .gesture(
+        DragGesture()
+          .updating($offset, body: { value, out, _ in
+            // offset을 업데이트
+            out = value.translation.width
+          })
+          .onEnded { value in
+            let offset = value.translation.width
+
+            // 전체 넓이 대비 드래그한 비율을 계산하고,
+            // 해당 비율을 반올림하여 가까운 Carousel 아이템으로 이동
+            let progress = -offset / width
+            let roundedIndex = progress.rounded()
+
+            let targetIndex = currentIndex + Int(roundedIndex)
+            var targetIndexInRange = min(
+              currentIndex + Int(roundedIndex),
+              items.count - 1
+            )
+            targetIndexInRange = max(targetIndexInRange, 0)
+
+            currentIndex = targetIndexInRange
+          }
+      )
+    }
+    // 손을 놓았을때 offset이 0이 되니 자연스럽게 애니메이션되어 원래 위치로.
+    .animation(.easeInOut, value: offset == 0)
+  }
+}
+
+struct CarouselView_Previews: PreviewProvider {
+  static var previews: some View {
+    CarouselViewPreView()
+  }
+}
+
+
+struct CarouselViewPreView: View {
+  @State var index = 0
+  let items = SearchResult.mock.screenshotUrls
+  let ratio = SearchResult.mock.screenShotMode.ratio
+  let width = UIScreen.main.bounds.width * 0.7
+
+  var body: some View {
+    Carousel(items: items, index: $index) { item in
+      RemoteImage(url: item) { image in
+        image
+          .resizable()
+      }
+      .frame(width: width, height: width * ratio)
+    }
+  }
+}
