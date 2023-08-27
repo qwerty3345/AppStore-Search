b521699df8d2ae7f03c0cce97686d95f3ae79f71
Mason Kim <qwerty3345@naver.com>
2023-08-27 16:12:10 +0900

    [Test] SearchReducer 테스트 케이스 추가
    - test_onAppear가_호출되면_히스토리를_불러온다
    - test_change가_호출되면_검색텍스트를_변경하고_상태를_검색중으로_변경한다
    - test_fetchComplete가_호출되면_검색결과를_저장하고_상태를_결과보여주기로_변경한다

--- Tests/SearchReducerTests/SearchReducerTests.swift
+++ Tests/SearchReducerTests/SearchReducerTests.swift
@@ -14,6 +14,7 @@ final class SearchReducerTests: XCTestCase {
   var historyService: MockHistoryService!
   var searchService: MockSearchService!
   var reducer: SearchReducer!
+  var store: StoreOf<SearchReducer>!
 
   override func setUpWithError() throws {
     historyService = MockHistoryService()
@@ -30,5 +31,35 @@ final class SearchReducerTests: XCTestCase {
     )
 
     reducer = SearchReducer()
+    store = Store(reducer: reducer)
+  }
+
+  override func tearDownWithError() throws {
+    historyService = nil
+    searchService = nil
+    reducer = nil
+    store = nil
+  }
+
+  func test_onAppear가_호출되면_히스토리를_불러온다() {
+    historyService.mockHistories = ["apple", "banana"]
+    store.dispatch(.onAppear)
+    XCTAssertEqual(store.state.histories, ["apple", "banana"])
+  }
+
+  func test_change가_호출되면_검색텍스트를_변경하고_상태를_검색중으로_변경한다() {
+    let searchText = "apple"
+    store.dispatch(.change(searchText: searchText))
+
+    XCTAssertEqual(store.state.searchText, searchText)
+    XCTAssertEqual(store.state.showingState, .searching)
+  }
+
+  func test_fetchComplete가_호출되면_검색결과를_저장하고_상태를_결과보여주기로_변경한다() {
+    let results = searchService.mockSearchResults
+    store.dispatch(.fetchComplete(results: results))
+
+    XCTAssertEqual(store.state.searchResults, results)
+    XCTAssertEqual(store.state.showingState, .showingResult)
   }
 }
