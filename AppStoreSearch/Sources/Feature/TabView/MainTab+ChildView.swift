1a67851bafe7000586d42c13cf7dfdbfc1105ea2
Mason Kim <qwerty3345@naver.com>
2023-08-27 16:37:58 +0900

    [Refactor] 메인탭에서 자식 뷰들 생성하는 부분 extension 파일 분리

---
+++ AppStoreSearch/Sources/Feature/TabView/MainTab+ChildView.swift
@@ -0,0 +1,30 @@
+//
+//  MainTab+ChildView.swift
+//  AppStoreSearch
+//
+//  Created by Mason Kim on 2023/08/27.
+//
+
+import SwiftUI
+import Core
+
+extension MainTab {
+  @ViewBuilder
+  var view: some View {
+    switch self {
+    case .today: placeholderView
+    case .game: placeholderView
+    case .app: placeholderView
+    case .arcade: placeholderView
+    case .search: SearchView()
+        .environmentObject(
+          Store(reducer: SearchReducer())
+        )
+    }
+  }
+
+  private var placeholderView: some View {
+    Text("빈 화면입니다.\n검색으로 이동 해 주세요.")
+      .multilineTextAlignment(.center)
+  }
+}
